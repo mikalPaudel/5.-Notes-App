5b9d15ba825b0220e1fb6dfed6b6c658e0593a7d
Mikal <mikalpaudel58@gmail.com>
2025-09-06 00:48:52 +0545

    refactor: comment out unused code and improve note creation logic

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const notesContainer = document.querySelector('.notes-container');
-const createNoteBtn = document.querySelector('.btn');
+const createBtn = document.querySelector('.btn');
 const notes = document.querySelector('.input-box');
-const dltBtn = document.getElementById('dlt');
+// const dltBtn = document.getElementById('dlt');
+
+createBtn.addEventListener('click', function() {
+    let inputBox = document.createElement('p');
+    let img = document.createElement('img');
+    inputBox.className= 'input-box';
+    inputBox.setAttribute('contenteditable', 'true');
+    img.src = "images/delete.png";
+    // inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+})
