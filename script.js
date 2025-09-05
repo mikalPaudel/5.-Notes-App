a15dfe3e2d23f085e1db58320aa74c71744ef5e2
Mikal <mikalpaudel58@gmail.com>
2025-09-06 00:19:12 +0545

    Add notes image to the project

---
+++ script.js
@@ -0,0 +1,4 @@
+const notesContainer = document.querySelector('.notes-container');
+const createNoteBtn = document.querySelector('.btn');
+const notes = document.querySelector('.input-box');
+const dltBtn = document.getElementById('dlt');
