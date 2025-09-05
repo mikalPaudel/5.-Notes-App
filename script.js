f4ef8ed02b97c91d8130183b5cea8971dbc79c02
Mikal <mikalpaudel58@gmail.com>
2025-09-06 01:08:55 +0545

    refactor: enhance note creation and storage functionality

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
 const notes = document.querySelector('.input-box');
-// const dltBtn = document.getElementById('dlt');
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem('notes');
+}
+showNotes();
+function updateStorage() {
+    localStorage.setItem('notes', notesContainer.innerHTML);
+}
 createBtn.addEventListener('click', function() {
     let inputBox = document.createElement('p');
     let img = document.createElement('img');
     inputBox.className= 'input-box';
     inputBox.setAttribute('contenteditable', 'true');
     img.src = "images/delete.png";
-    // inputBox.appendChild(img);
+    inputBox.appendChild(img);
     notesContainer.appendChild(inputBox).appendChild(img);
-
+    updateStorage();
+    inputBox.focus();
 })
+
+notesContainer.addEventListener('input', function(e) {
+    if(e.target.classList.contains('input-box'))
+    {
+        updateStorage();
+    }
+})
+
+
+notesContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === 'IMG') {
+        e.target.parentElement.remove();
+        updateStorage();
+    }   
+    else if(e.target.tagName === 'P') {
+        notes = document.querySelectorAll('.input-box');
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Enter' && e.target.classList.contains('input-box')) {
+        document.execCommand('insertLineBreak');
+        e.preventDefault();
+    }
+})
+showNotes();
